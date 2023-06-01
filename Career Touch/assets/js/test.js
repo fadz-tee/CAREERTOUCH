97b5c1bc338a91da176a6da2ebf27a4905f71937
Gbadebo Toyin Mary <fadipetoyinmary@gmail.com>
2023-06-01 15:54:03 +0100

    chnage made

---
+++ Career Touch/assets/js/test.js
@@ -0,0 +1,10 @@
+const usersReview = [
+    { userName: "Toyin", image: "https://pathto/picture", reviewText: "Thank you ..." },
+    { userName: "Toyin", image: "https://pathto/picture", reviewText: "Thank you ..." },
+    { userName: "Toyin", image: "https://pathto/picture", reviewText: "Thank you ..." }, 
+    { userName: "Toyin", image: "https://pathto/picture", reviewText: "Thank you ..." }
+]
+
+usersReview.forEach((review) => {
+    console.log("<li>")
+})
